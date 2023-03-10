bbe5442c0495727751a74dbc5a37a44a6b5ebf63
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-03-10 13:14:59 -0500

    added 2 single fish, and a school of 3. All animated. Animated bubble column and plants
    I reused my fish, and background sprites from the first fish assignment, but the bubbles, and plants I drew

---
+++ app.js
@@ -0,0 +1,14 @@
+class bubbleColumn {
+
+    bubbleColumn(spriteNum, speed, decayTime) {
+        this.spriteNum = spriteNum;
+        this.speed = speed;
+        this.decayTime = decayTime;
+    }
+
+    start() {
+        let particles = [];
+        for (let i = 0; i < this.spriteNum; i++)
+    }
+
+}
